f10dcddf92d9aa1f9226565d7283c3fa5a06d8a8
JCBEA <111287289+JCBEA@users.noreply.github.com>
2022-11-19 16:26:24 +0800

    bago nanaman

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Database, ref, set} from '@angular/fire/database';
+import { Auth, createUserWithEmailAndPassword, linkWithRedirect, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public auth: Auth, public database: Database,
+    public route:Router) {
+  }
   ngOnInit(): void {
   }
 
+
+
+  registerUser(value: any) {
+    //create data
+    set(ref(this.database, 'Books/' + value.title), {
+     title: value.title,
+      story: value.story,
+       published : value.published,
+       author: value.author
+     }); 
+     alert('user created!');
+}
+signOut() {
+  return this.auth.signOut().then(() => {
+    window.alert('Logged out!');
+    this.route.navigate(['/admin-login'])
+  });
+}
 }

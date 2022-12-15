26ea1617dc9d1604379099fc53b2d3a73b014cec
JCBEA <111287289+JCBEA@users.noreply.github.com>
2022-12-15 14:25:03 +0800

    alerts/ comment/ delete comment

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Database, set, ref } from '@angular/fire/database';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-contact-us',
@@ -15,13 +16,13 @@ export class ContactUsComponent implements OnInit {
     email: new FormControl('', Validators.required),
     comment: new FormControl('', Validators.required)
   })
-
   constructor(public database: Database) { }
 
   ngOnInit() {
     this.resetForm();
+    
   }
-
+  
   registerUser(value:any){
     this.isSubmitted = true;
     if (this.formTemplate.valid) {
@@ -29,9 +30,19 @@ export class ContactUsComponent implements OnInit {
       name: value.name,
       Email: value.email,
       comment: value.comment,
-      
+      month: new Date().getMonth() + 1,
+      Year: new Date().getFullYear(),
+      day: new Date().getDate(),
+      hours: new Date().getHours(),
+      minutes: new Date().getMinutes()
     });
-    alert('Successfuly Commented!!');
+    Swal.fire({    
+      title: 'Thank you for your feedback',
+      icon:'success',
+      color: 'black',
+      confirmButtonText: 'okay',
+      position: 'top-right'
+  })
     this.resetForm();
     }}
     get formControls() {
